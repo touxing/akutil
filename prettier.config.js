4c7a36466af40f75a293e61d2f0030e1ffdb10db
hotsuitor <hotsuitor@qq.com>
2022-10-25 18:36:49 +0800

    chore: git hook test

--- prettier.config.js
+++ prettier.config.js
@@ -6,4 +6,24 @@ module.exports = {
   printWidth: 80,
   tabWidth: 2,
   arrowParens: 'always',
+  overrides: [
+    {
+      files: ['*.json'],
+      options: {
+        parser: 'json5',
+      },
+    },
+    {
+      files: ['*.yml', '*.yaml'],
+      options: {
+        parser: 'yaml',
+      },
+    },
+    {
+      files: ['*.md'],
+      options: {
+        parser: 'markdown',
+      },
+    },
+  ],
 }
