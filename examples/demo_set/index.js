34fcb02286069c1e4665c644d2a1f48665e54f1d
hotsuitor <hotsuitor@qq.com>
2022-11-18 17:39:26 +0800

    feat: Set集合运算

--- examples/demo_set/index.js
+++ examples/demo_set/index.js
@@ -16,3 +16,27 @@ console.log(set.values())
 
 console.log(set.delete(3))
 console.log(set.values())
+
+const setA = new akutil.Set()
+const setB = new akutil.Set()
+
+setA.add(1)
+setA.add(2)
+setA.add(3)
+
+setB.add(3)
+setB.add(4)
+setB.add(5)
+
+const unionAB = setA.union(setB)
+console.log('A', setA.values());
+console.log('B', setB.values());
+console.log('A 并集 B',unionAB.values())
+console.log('A 交集 B',setA.intersection(setB).values())
+console.log('A 差集 B', setA.difference(setB).values())
+
+const setC = new akutil.Set()
+setC.add(1)
+
+console.log('C is A 的子集', setC.isSubsetOf(setA));
+console.log('B is A 的子集', setB.isSubsetOf(setA));
--- src/struct/Set.ts
+++ src/struct/Set.ts
@@ -1,8 +1,9 @@
-interface SetType {
+interface SetContainerType {
   [index: string]: any
 }
+
 export class Set {
-  protected items: SetType
+  protected items: SetContainerType
   constructor() {
     this.items = {}
   }
@@ -77,7 +78,82 @@ export class Set {
     return values
   }
 
+  /**
+   * clear all element
+   * @returns void
+   */
   clear() {
     this.items = {}
+  }
+
+  /**
+   * 并集运算
+   * @param otherSet
+   * @returns {Set} 并集运算后的集合
+   */
+  union(otherSet: Set): Set {
+    const unionSet = new Set()
+    this.values().forEach((value) => unionSet.add(value))
+    otherSet.values().forEach((value) => unionSet.add(value))
+    return unionSet
+  }
+
+  /**
+   * 交集运算
+   * @param {Set} otherSet
+   * @returns 交集
+   */
+  intersection(otherSet: Set): Set {
+    const intersectionSet = new Set()
+    let biggerSet = this
+    let smallerSet = otherSet
+    if (otherSet.size() - this.size() > 0) {
+      biggerSet = this
+      smallerSet = otherSet
+    }
+    smallerSet.values().forEach((value) => {
+      if (biggerSet.has(value)) {
+        intersectionSet.add(value)
+      }
+    })
+
+    return intersectionSet
+  }
+
+  /**
+   * 差集运算
+   * @param {Set} otherSet
+   * @returns 差集
+   */
+  difference(otherSet: Set): Set {
+    const differenceSet = new Set()
+    this.values().forEach((value) => {
+      if (!otherSet.has(value)) {
+        differenceSet.add(value)
+      }
+    })
+
+    return differenceSet
+  }
+
+  /**
+   * 判断是否子集
+   * @param {Set} otherSet
+   * @returns if 子集 return true, else return false
+   */
+  isSubsetOf(otherSet: Set): boolean {
+    // 集合数量比判断的集集多，肯定不是子集
+    if (this.size() > otherSet.size()) return false
+
+    let isSubset = true
+    this.values().every((value) => {
+      if (!otherSet.has(value)) {
+        isSubset = false
+        return false
+      }
+      return true
+    })
+
+    return isSubset
   }
 }
